2229763223f962daeeee1a750432614d48cd88d0
cetinyavuz1 <cetinyavuz1@hotmail.com>
2025-05-25 01:46:37 +0300

    Update Login.jsx

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,6 +1,42 @@
+import { Form, FormGroup, Input, Label, Button } from "reactstrap";
+
 export default function Login() {
-    return(
-        <>
-        </>
-    )
+  return (
+    <>
+      <Form>
+        <FormGroup>
+          <Label for="exampleEmail">
+            Email
+          </Label>
+          <Input
+            id="exampleEmail"
+            name="email"
+            placeholder="with a placeholder"
+            type="email"
+          />
+        </FormGroup>
+        <FormGroup>
+          <Label for="examplePassword">
+            Password
+          </Label>
+          <Input
+            id="examplePassword"
+            name="password"
+            placeholder="password placeholder"
+            type="password"
+          />
+        </FormGroup>
+        <FormGroup check>
+          <Input type="checkbox" id="terms" />
+          {' '}
+          <Label check htmlFor="terms">
+            Check me out
+          </Label>
+        </FormGroup>
+        <Button>
+          Login
+        </Button>
+      </Form>
+    </>
+  )
 }
