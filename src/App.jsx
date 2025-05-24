3e749dbf55d5585aee35803dc2d6870ecd9f000b
cetinyavuz1 <cetinyavuz1@hotmail.com>
2025-05-25 01:42:30 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
-
-
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Login from "./components/Login.jsx"
 
 function App() {
 
 
   return (
     <>
-      
+      <Login />
     </>
   )
 }
